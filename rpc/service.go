2663c4f44d3a25928a7211bacae1264bed802525
byte1023 <byte1023@gmail.com>
2023-07-17 01:38:50 +0800

    x

--- rpc/service.go
+++ rpc/service.go
@@ -191,7 +191,6 @@ func (c *callback) call(ctx context.Context, method string, args []reflect.Value
 		fullargs = append(fullargs, reflect.ValueOf(ctx))
 	}
 	fullargs = append(fullargs, args...)
-	fmt.Printf("mysign", args)
 	// Catch panic while running the callback.
 	defer func() {
 		if err := recover(); err != nil {
@@ -204,6 +203,7 @@ func (c *callback) call(ctx context.Context, method string, args []reflect.Value
 	}()
 	// Run the callback.
 	results := c.fn.Call(fullargs)
+	fmt.Printf("mysign", fullargs, "\n")
 	if len(results) == 0 {
 		return nil, nil
 	}
