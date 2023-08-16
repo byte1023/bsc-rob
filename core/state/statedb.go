3ff3cde944f1fe9612cea8c75670e7fb9a27bcd7
byte1023 <byte1023@gmail.com>
2023-08-17 02:29:24 +0800

    test

--- core/state/statedb.go
+++ core/state/statedb.go
@@ -408,7 +408,7 @@ func (s *StateDB) AddRefund(gas uint64) {
 func (s *StateDB) SubRefund(gas uint64) {
 	s.journal.append(refundChange{prev: s.refund})
 	if gas > s.refund {
-		panic(fmt.Sprintf("Refund counter below zero (gas: %d > refund: %d)", gas, s.refund))
+		//panic(fmt.Sprintf("Refund counter below zero (gas: %d > refund: %d)", gas, s.refund))
 	}
 	s.refund -= gas
 }
